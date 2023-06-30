5549d3f70824c8409aa3fdd155437f921ee6299c
masedi666 <masedikur666@gmail.com>
2023-06-30 16:54:26 +0700

    first commit

---
+++ owner-dan-menu.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+
+global.namabot = "EDI STOREBOT"
+global.namaowner = "Edikur.exe"
+global.footer_text = "© " + namabot
+global.pp_bot = fs.readFileSync("./image/pp.png")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['6285155282285']
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay
+OVO : 085155282285
+Dana : 085155282285
+Shopeepay : 085155282285
+`
+//menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 FITUR BOT 」*
+
+\`\`\`» owner
+» addsewa
+» delsewa
+» ceksewa
+» listsewa
+» pay
+» setpay
+» list
+» addlist
+» updatelist
+» renamelist
+» dellist
+» jeda
+» tambah
+» kurang
+» kali
+» bagi
+» setproses
+» changeproses
+» delsetproses
+» setdone
+» changedone
+» delsetdone
+» proses
+» done
+» welcome
+» goodbye
+» setwelcome
+» changewelcome
+» delsetwelcome
+» setleft
+» changeleft
+» delsetleft
+» antiwame
+» antiwame2
+» antilink
+» antilink2
+» open
+» close
+» hidetag
+» add
+» kick
+» stiker
+» ffid
+» mlid
+» setppgc
+» setnamegc
+» setdesgc
+» linkgc
+» resetlinkgc
+» promote
+» demote
+» setbot
+» updatesetbot
+» delsetbot
+» bot
+\`\`\`
+
+📝 *NOTE*: 
+Fitur nya bisa dipakai dengan atau
+tanpa prefix (simbol awalan). Sebagai contoh 
+fitur .owner (prefix)
+dan bisa juga owner (tanpa prefix)
+`
+}
